e3115a4b41d13f3c73205f069de86d19f26483b6
TharinduDeshan <tharindudeshan50@gmail.com>
2021-09-24 20:48:23 +0530

    Tharindu branch commit 3

---
+++ app/src/main/java/com/example/mad_group_project/Contact.java
@@ -0,0 +1,35 @@
+package com.example.mad_group_project;
+
+public class Contact {
+
+    private String Name;
+    private String Email;
+    private String Massage;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getMassage() {
+        return Massage;
+    }
+
+    public void setMassage(String massage) {
+        Massage = massage;
+    }
+}
