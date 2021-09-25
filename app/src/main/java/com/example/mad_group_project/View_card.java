3499b49bbe69311bcea90e2899fad3f10127d3d4
DeeghayuAlwis <dcdalwis99@gmail.com>
2021-09-25 20:16:32 +0530

    Adding View Card

---
+++ app/src/main/java/com/example/mad_group_project/View_card.java
@@ -0,0 +1,14 @@
+package com.example.mad_group_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_card);
+    }
+}
