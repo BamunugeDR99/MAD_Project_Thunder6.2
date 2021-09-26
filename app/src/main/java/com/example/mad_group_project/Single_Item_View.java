357a7b4cf2326b71f29e8071b64976abd8959c8c
BamunugeDR <drbamunuge666@gmail.com>
2021-09-26 12:25:54 +0530

    Dulan Branch itemView interface

---
+++ app/src/main/java/com/example/mad_group_project/Single_Item_View.java
@@ -0,0 +1,14 @@
+package com.example.mad_group_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Single_Item_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_item_view);
+    }
+}
