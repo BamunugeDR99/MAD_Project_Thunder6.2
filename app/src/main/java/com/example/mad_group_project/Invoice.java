18cf61dee992478fc324d34c9b9f1e52aef1124c
Vidusha Hirushan <hirushanvidusha@gmail.com>
2021-09-26 10:50:18 +0530

    Shopping Cart 1

---
+++ app/src/main/java/com/example/mad_group_project/Invoice.java
@@ -0,0 +1,13 @@
+package com.example.mad_group_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Invoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invoice);
+    }
+}
