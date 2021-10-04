14b17f4716406fee7219cd424985bb7d42a02fd9
BamunugeDR <drbamunuge666@gmail.com>
2021-10-04 20:28:55 +0530

    Dulan Branch  add Card Correction

--- app/src/main/java/com/example/mad_group_project/Add_New_Payment.java
+++ app/src/main/java/com/example/mad_group_project/Add_New_Payment.java
@@ -1,8 +1,5 @@
 package com.example.mad_group_project;
-
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,18 +8,11 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.HashMap;
 import java.util.Map;
-
-import com.basgeekball.awesomevalidation.AwesomeValidation;
-import com.basgeekball.awesomevalidation.ValidationStyle;
-import com.basgeekball.awesomevalidation.utility.RegexTemplate;
-
 public class Add_New_Payment extends AppCompatActivity {
 
 
@@ -30,69 +20,29 @@ public class Add_New_Payment extends AppCompatActivity {
     private RadioButton radioButton;
     EditText AC_Card_Owner,AC_Card_Number,AC_Card_Date;
     Button AC_BTN_Submit;
-
-    AwesomeValidation awesomeValidation;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_payment);
-
-
-        radioGroup = (RadioGroup) findViewById(R.id.radio);
-
+        radioGroup = (RadioGroup) findViewById(R.id.RadioGroup);
         AC_Card_Owner=(EditText) findViewById(R.id.AC_Card_Owner);
         AC_Card_Number=(EditText) findViewById(R.id.AC_Card_Number);
         AC_Card_Date=(EditText) findViewById(R.id.AC_Card_Date);
-
         AC_BTN_Submit=(Button) findViewById(R.id.AC_BTN_Submit);
-
-        //Initialize Validation Style
-        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
-
-
-        //Validate AC_Card_Number
-        awesomeValidation.addValidation(this,R.id.AC_Card_Number,
-                "[0-9]{16}$",R.string.invalid_number);
-        //Validate AC_Card_Owner
-        awesomeValidation.addValidation(this,R.id.AC_Card_Owner,
-                RegexTemplate.NOT_EMPTY,R.string.invalid_name);
-        //Validate AC_Card_Date
-        awesomeValidation.addValidation(this,R.id.AC_Card_Date,
-                RegexTemplate.NOT_EMPTY,R.string.invalid_date);
-
-
         AC_BTN_Submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                //Check Validation
-                if (awesomeValidation.validate()) {
-                    //On Success
-                    Toast.makeText(getApplicationContext(), "Data Validated Successfully!!",Toast.LENGTH_SHORT).show();
-                    insertData();
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "Data Validation Failed!!",Toast.LENGTH_SHORT).show();
-                }
-
 
 
 
 
                 // get selected radio button from radioGroup
                 int selectedId = radioGroup.getCheckedRadioButtonId();
-
                 // find the radiobutton by returned id
                 radioButton = (RadioButton) findViewById(selectedId);
-
-
-
                 insertData();
-
             }
         });
-
     }
 
     public void ACClearALL(){
@@ -100,41 +50,37 @@ public class Add_New_Payment extends AppCompatActivity {
         AC_Card_Owner.setText("");
         AC_Card_Number.setText("");
         AC_Card_Date.setText("");
-
     }
 
     private  void insertData()
     {
         String CardImg = "";
-
         Toast.makeText(Add_New_Payment.this,
                 radioButton.getText(), Toast.LENGTH_SHORT).show();
-
-
         Map<String, Object> map = new HashMap<>();
-
         Log.d("CardType", radioButton.getText().toString());
-
         if(radioButton.getText().toString().equalsIgnoreCase("Master card")){
 
             CardImg = "https://wallpapercave.com/wp/wp4750374.png";
             map.put("image","https://wallpapercave.com/wp/wp4750374.png");
+            map.put("CardType", "Master Card");
         }
 
         else if(radioButton.getText().toString().equalsIgnoreCase("Visa") ){
 
             CardImg = "https://banner2.cleanpng.com/20180203/yve/kisspng-visa-credit-card-gift-card-payment-cheque-mastercard-cliparts-5a7671934cdd86.2689827715177117633149.jpg";
             map.put("image", "https://banner2.cleanpng.com/20180203/yve/kisspng-visa-credit-card-gift-card-payment-cheque-mastercard-cliparts-5a7671934cdd86.2689827715177117633149.jpg");
+            map.put("CardType", "Visa");
+
         }
 
 
 
 
         map.put("name", AC_Card_Owner.getText().toString());
-        map.put("number", Long.parseLong(AC_Card_Number.getText().toString()));
+        map.put("number", (AC_Card_Number.getText().toString()));
         map.put("date", AC_Card_Date.getText().toString());
-
-        FirebaseDatabase.getInstance().getReference().child("Card Details").push()
+        FirebaseDatabase.getInstance().getReference().child("Card Details").child("Cus1").push()
                 .setValue(map)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
@@ -142,7 +88,6 @@ public class Add_New_Payment extends AppCompatActivity {
 
                         Toast.makeText(Add_New_Payment.this, "Data Inserted Successfully", Toast.LENGTH_SHORT).show();
                         ACClearALL();
-
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -152,7 +97,5 @@ public class Add_New_Payment extends AppCompatActivity {
                         Toast.makeText(Add_New_Payment.this, "Error while Insertion", Toast.LENGTH_SHORT).show();
                     }
                 });
-
-
     }
 }
